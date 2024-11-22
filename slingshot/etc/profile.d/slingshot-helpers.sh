4ae2f01af593023a8efc646a62859355c539cc40
celenity <celenity@celenity.dev>
2024-11-22 16:05:59 -0500

    Tweaks & minor fixes
    Signed-off-by: celenity <celenity@celenity.dev>

--- slingshot/etc/profile.d/slingshot-helpers.sh
+++ slingshot/etc/profile.d/slingshot-helpers.sh
@@ -130,7 +130,8 @@ alias ssh-tor='torsocks --isolate ssh';
 alias dnf-update-security-testing="sudo dnf update --refresh --security --enablerepo=*updates-testing";
 alias dnf-update-kernel="sudo dnf update --enablerepo=*updates-testing kernel*"; #sometimes testing kernels aren't flagged as security updates
 
-alias update="sudo dnf update --refresh && flatpak --system update && flatpak --user update && sudo fwupdmgr refresh --force && sudo fwupdmgr upgrade && sudo akmods --force";
+alias update="sudo dnf update --refresh && flatpak --system update && flatpak --user update && sudo fwupdmgr refresh --force && sudo fwupdmgr upgrade";
+alias updatenv="sudo dnf update --refresh && flatpak --system update && flatpak --user update && sudo fwupdmgr refresh --force && sudo fwupdmgr upgrade && sudo akmods --force";
 alias envedit="nano ~/.bashrc";
 alias envup="source ~/.bashrc";
 alias slingshot-test="echo Roger that.";
